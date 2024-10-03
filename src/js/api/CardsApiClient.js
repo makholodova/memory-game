39dc2882f05d32b8d569c3a89d571b24c0f48f26
Marina Kholodova <marinaholodova@gmail.com>
2024-10-03 23:09:12 +0300

    feat: get dates from dataBase

---
+++ src/js/api/CardsApiClient.js
@@ -0,0 +1,16 @@
+﻿export class CardsApiClient {
+	async getCards() {
+		try {
+			const response = await fetch('./data/cards.json');
+			if (!response.ok) {
+				console.error(`Ошибка сети: ${response.status}`);
+				return [];
+			}
+			return await response.json();
+		}
+		catch (error) {
+			console.error('Ошибка загрузки данных:', error);
+			return [];
+		}
+	}
+}
