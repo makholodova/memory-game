96d29fe5b493d35e9a1bb95321f5fcd63922d91e
Marina Kholodova <marinaholodova@gmail.com>
2024-10-03 23:09:53 +0300

    feat: add localStorage

---
+++ src/store/playersStore.js
@@ -0,0 +1,24 @@
+﻿const playersArray = getPlayers();
+export function getPlayers() {
+	try {
+		const items = localStorage.getItem('players');
+		return items ? JSON.parse(items) : [];
+	}
+	catch (error) {
+		console.error('Error parsing todos from localStorage:', error);
+		return [];
+	}
+}
+export function showPlayers() {
+	return playersArray.slice(-10).reverse();
+}
+export function addPlayer(player) {
+	playersArray.push(player);
+	saveToLocalStorage();
+}
+function saveToLocalStorage() {
+	localStorage.setItem('players', JSON.stringify(playersArray));
+}
+/*function clearLocalStorage(): void {
+    localStorage.clear();
+}*/
